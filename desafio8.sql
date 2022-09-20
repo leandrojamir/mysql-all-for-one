7bca10d5fc2fc8f7f7bbd98a9f081679ca75f8e5
leandrojamir <leandrojamir@hotmail.com>
2022-09-19 22:25:55 -0300

    adcionando req-8 teste local ok

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+-- 8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
+-- Observações técnicas
+-- Na primeira coluna, exiba a soma de 5 + 6 (essa soma deve ser realizada pelo SQL);
+-- Na segunda coluna deve haver a palavra "de";
+-- E por fim, na terceira coluna, exiba a soma de 2 + 8 (essa soma deve ser realizada pelo SQL);
+-- A primeira coluna deve se chamar "A", a segunda coluna deve se chamar "Trybe" e a terceira coluna deve se chamar "eh";
+-- Não use colunas pré-existentes, apenas o que for criado na hora.
+SELECT ( 5 + 6 ) A, 'de' Trybe, ( 2 + 8 ) eh FROM northwind.products LIMIT 1;
