f5a9e0962e589cc7e77012652ba25d87f57a59c1
leandrojamir <leandrojamir@hotmail.com>
2022-09-20 21:52:59 -0300

    adcionando req-27, DELETE sem WHERE! rsrs, todos teste 100% ok

---
+++ desafio27.sql
@@ -0,0 +1,6 @@
+-- 27 - Delete (todos) os dados da tabela (order_details).
+-- Example
+-- DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
+-- DELETE FROM northwind.order_details WHERE unit_price < 10.000;
+-- Neste caso pode usar o famoso DELETE sem WHERE
+DELETE FROM northwind.order_details;
